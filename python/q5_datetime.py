667e402017793572f2d189d0618f031649913766
Kailin Xie <bead8@hotmail.com>
2017-03-21 23:17:51 -0700

    Update q5_datetime.py

--- python/q5_datetime.py
+++ python/q5_datetime.py
@@ -5,7 +5,7 @@ date_start = '01-02-2013'
 date_stop = '07-28-2015'   
 from datetime import datetime  
 def delta(date1, date2):  
-	date1 = datetime.strptime(date1, "%m-%d-%Y")  
+    date1 = datetime.strptime(date1, "%m-%d-%Y")  
     date2 = datetime.strptime(date2, "%m-%d-%Y")  
     return (date2 - date1).days  
 print(delta(date_start, date_stop))
@@ -15,9 +15,9 @@ date_start = '12312013'
 date_stop = '05282015'  
 from datetime import datetime  
 def delta(date1, date2):  
-	date1 = datetime.strptime(date1, "%m%d%Y")  
-	date2 = datetime.strptime(date2, "%m%d%Y")  
-	return (date2 - date1).days  
+    date1 = datetime.strptime(date1, "%m%d%Y")  
+    date2 = datetime.strptime(date2, "%m%d%Y")  
+    return (date2 - date1).days  
 print(delta(date_start, date_stop))
 
 ####c)  
@@ -25,7 +25,7 @@ date_start = '15-Jan-1994'
 date_stop = '14-Jul-2015'  
 from datetime import datetime  
 def delta(date1, date2):  
-	date1 = datetime.strptime(date1, "%d-%b-%Y")  
-	date2 = datetime.strptime(date2, "%d-%b-%Y")  
-	return (date2 - date1).days  
+    date1 = datetime.strptime(date1, "%d-%b-%Y")  
+    date2 = datetime.strptime(date2, "%d-%b-%Y")  
+    return (date2 - date1).days  
 print(delta(date_start, date_stop))
