949005551cc1d68c7c5e97a2c66ff7766f313fe1
Kailin Xie <bead8@hotmail.com>
2017-03-22 16:17:30 -0700

    Update

--- python/q5_datetime.py
+++ python/q5_datetime.py
@@ -8,7 +8,8 @@ def delta(date1, date2):
     date1 = datetime.strptime(date1, "%m-%d-%Y")  
     date2 = datetime.strptime(date2, "%m-%d-%Y")  
     return (date2 - date1).days  
-print(delta(date_start, date_stop))
+
+delta(date_start, date_stop)
 
 ####b)  
 date_start = '12312013'  
@@ -18,7 +19,8 @@ def delta(date1, date2):
     date1 = datetime.strptime(date1, "%m%d%Y")  
     date2 = datetime.strptime(date2, "%m%d%Y")  
     return (date2 - date1).days  
-print(delta(date_start, date_stop))
+
+delta(date_start, date_stop)
 
 ####c)  
 date_start = '15-Jan-1994'  
@@ -28,4 +30,5 @@ def delta(date1, date2):
     date1 = datetime.strptime(date1, "%d-%b-%Y")  
     date2 = datetime.strptime(date2, "%d-%b-%Y")  
     return (date2 - date1).days  
-print(delta(date_start, date_stop))
+
+delta(date_start, date_stop)
