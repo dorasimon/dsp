3d5a2e33486a42f1bbaf1df4f79ff756fca84614
Kailin Xie <dorasimon@Kailins-MacBook-Air.local>
2017-03-24 13:25:49 -0700

    Update

--- python/advanced_python_csv.py
+++ python/advanced_python_csv.py
@@ -1,44 +1,44 @@
 import csv
 
-with open("emails.csv", "w") as file:
-    fieldnames = ["email"]
-    writer = csv.DictWriter(file, fieldnames = fieldnames)
-
-    writer.writeheader()
-    writer.writerow({"email": 'bellamys@mail.med.upenn.edu'})
-    writer.writerow({"email": 'warren@upenn.edu'})
-    writer.writerow({"email": 'bryanma@upenn.edu'})
-    writer.writerow({"email": 'jinboche@upenn.edu'})
-    writer.writerow({"email": 'sellenbe@upenn.edu'})
-    writer.writerow({"email": 'jellenbe@mail.med.upenn.edu'})
-    writer.writerow({"email": 'ruifeng@upenn.edu'})
-    writer.writerow({"email": 'bcfrench@mail.med.upenn.edu'})
-    writer.writerow({"email": 'pgimotty@upenn.edu'})
-    writer.writerow({"email": 'wguo@mail.med.upenn.edu'})
-    writer.writerow({"email": 'hsu9@mail.med.upenn.edu'})
-    writer.writerow({"email": 'rhubb@mail.med.upenn.edu'})
-    writer.writerow({"email": 'whwang@mail.med.upenn.edu'})
-    writer.writerow({"email": 'mjoffe@mail.med.upenn.edu'})
-    writer.writerow({"email": 'jrlandis@mail.med.upenn.edu'})
-    writer.writerow({"email": 'liy3@email.chop.edu'})
-    writer.writerow({"email": 'mingyao@mail.med.upenn.edu'})
-    writer.writerow({"email": 'hongzhe@upenn.edu'})
-    writer.writerow({"email": 'rlocalio@upenn.edu'})
-    writer.writerow({"email": 'nanditam@mail.med.upenn.edu'})
-    writer.writerow({"email": 'knashawn@mail.med.upenn.edu'})
-    writer.writerow({"email": 'propert@mail.med.upenn.edu'})
-    writer.writerow({"email": 'mputt@mail.med.upenn.edu'})
-    writer.writerow({"email": 'sratclif@upenn.edu'})
-    writer.writerow({"email": 'michross@upenn.edu'})
-    writer.writerow({"email": 'jaroy@mail.med.upenn.edu'})
-    writer.writerow({"email": 'msammel@cceb.med.upenn.edu'})
-    writer.writerow({"email": 'shawp@upenn.edu'})
-    writer.writerow({"email": 'rshi@mail.med.upenn.edu'})
-    writer.writerow({"email": 'hshou@mail.med.upenn.edu'})
-    writer.writerow({"email": 'jshults@mail.med.upenn.edu'})
-    writer.writerow({"email": 'alisaste@mail.med.upenn.edu'})
-    writer.writerow({"email": 'atroxel@mail.med.upenn.edu'})
-    writer.writerow({"email": 'rxiao@mail.med.upenn.edu'})
-    writer.writerow({"email": 'sxie@mail.med.upenn.edu'})
-    writer.writerow({"email": 'dxie@upenn.edu'})
-    writer.writerow({"email": 'weiyang@mail.med.upenn.edu'})
+with open("emails.csv", "w", newline = "") as file:
+    writer = csv.writer(file, delimiter = ",")
+    data = [
+    ["email"], 
+    ['bellamys@mail.med.upenn.edu'], 
+    ['warren@upenn.edu'], 
+    ['bryanma@upenn.edu'], 
+    ['jinboche@upenn.edu'], 
+    ['sellenbe@upenn.edu'], 
+    ['jellenbe@mail.med.upenn.edu'], 
+    ['ruifeng@upenn.edu'], 
+    ['bcfrench@mail.med.upenn.edu'], 
+    ['pgimotty@upenn.edu'], 
+    ['wguo@mail.med.upenn.edu'], 
+    ['hsu9@mail.med.upenn.edu'], 
+    ['rhubb@mail.med.upenn.edu'], 
+    ['whwang@mail.med.upenn.edu'], 
+    ['mjoffe@mail.med.upenn.edu'], 
+    ['jrlandis@mail.med.upenn.edu'], 
+    ['liy3@email.chop.edu'], 
+    ['mingyao@mail.med.upenn.edu'], 
+    ['hongzhe@upenn.edu'], 
+    ['rlocalio@upenn.edu'], 
+    ['nanditam@mail.med.upenn.edu'], 
+    ['knashawn@mail.med.upenn.edu'], 
+    ['propert@mail.med.upenn.edu'], 
+    ['mputt@mail.med.upenn.edu'], 
+    ['sratclif@upenn.edu'], 
+    ['michross@upenn.edu'], 
+    ['jaroy@mail.med.upenn.edu'], 
+    ['msammel@cceb.med.upenn.edu'], 
+    ['shawp@upenn.edu'], 
+    ['rshi@mail.med.upenn.edu'], 
+    ['hshou@mail.med.upenn.edu'], 
+    ['jshults@mail.med.upenn.edu'], 
+    ['alisaste@mail.med.upenn.edu'], 
+    ['atroxel@mail.med.upenn.edu'], 
+    ['rxiao@mail.med.upenn.edu'], 
+    ['sxie@mail.med.upenn.edu'], 
+    ['dxie@upenn.edu'], 
+    ['weiyang@mail.med.upenn.edu']]
+    writer.writerows(data)
