0b55e16ca5e9d78e7d44aa15cffeee21d655856c
Kailin Xie <dorasimon@Kailins-MacBook-Air.local>
2017-04-02 23:59:27 -0700

    Add answer

--- math/matrix_algebra.py
+++ math/matrix_algebra.py
@@ -1,3 +1,36 @@
 # Matrix Algebra
 
-PLACE YOUR CODE HERE
+import numpy as np
+
+A = np.array([[1, 2, 3], [2, 7, 4]])
+B = np.array([[1, -1], [0, 1]])
+C = np.array([[5, -1], [9, 1], [6, 0]])
+D = np.array([[3, -2, -1], [1, 2, 3]])
+u = np.array([6, 2, -3, 5])
+v = np.array([3, 5, -1, 4])
+w = np.array([[1], [8], [0], [5]])
+
+print(A.shape)
+print(B.shape)
+print(C.shape)
+print(D.shape)
+print(u.shape)
+print(v.shape)
+print(w.shape)
+
+print(u + v)
+print(u - v)
+print(np.dot(6, u))
+print(np.dot(u, v))
+print(np.sqrt(np.dot(u, u)))
+
+print(A + C)
+print(A - C.T)
+print(C.T + 3 * D)
+print(np.dot(B, A))
+print(np.dot(B, A.T))
+print(np.dot(B, C))
+print(np.dot(C, B))
+print(np.linalg.matrix_power(B, 4))
+print(np.dot(A, A.T))
+print(np.dot(D.T, D))
